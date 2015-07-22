b8ff532a6606a3536a7ed84221a4690986f2cb32
Gabriel Somoza <gabriel@somoza.me>
2015-07-23 00:35:54 +0200

    Changed Repository\ListCommand's optional parameter "reverse" to "newest-first" for better readability

--- src/Command/Repository/ListCommand.php
+++ src/Command/Repository/ListCommand.php
@@ -36,7 +36,7 @@ class ListCommand extends RepositoryCommand
     {
         $this->setDescription('Prints version IDs for all available migrations ordered incrementally.')
             ->addOption(
-                'reverse',
+                'newest-first',
                 null,
                 InputOption::VALUE_NONE,
                 'Sort list in reverse order (newest first)'
@@ -49,7 +49,7 @@ class ListCommand extends RepositoryCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $reverse = $input->getOption('reverse');
+        $reverse = $input->getOption('newest-first');
         $versions = $this->getCollection();
         if (count($versions) > 0) {
             if ($reverse) {
