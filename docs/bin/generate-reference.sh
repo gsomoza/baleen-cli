1c702515d67ad4b2847b702be9d7c12f922af5ec
Gabriel Somoza <gabriel@somoza.me>
2015-09-29 11:11:21 +0200

    Added requirements.txt file for pip, renamed reference to "ref" and refactored 3rd-party modules documentation location and contents.

--- docs/bin/generate-reference.sh
+++ docs/bin/generate-reference.sh
@@ -3,7 +3,7 @@ EXECPATH=`dirname $0`
 cd $EXECPATH
 cd ..
 
-rm -rf en/reference/Baleen
-mkdir -p en/reference/Baleen
-../vendor/bin/sphpdox process -o en/reference Baleen\\Cli ../src
-find en/reference -name "*.rst" -exec bash -c 'mv "$1" "$(sed "s/\.rst$/.txt/" <<< "$1")"' - '{}' \;
+rm -rf en/ref/Baleen
+mkdir -p en/ref/Baleen
+../vendor/bin/sphpdox process -o en/ref Baleen\\Cli ../src
+find en/ref -name "*.rst" -exec bash -c 'mv "$1" "$(sed "s/\.rst$/.txt/" <<< "$1")"' - '{}' \;
